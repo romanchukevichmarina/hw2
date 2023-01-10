dd317ff967ed14970f92095e2ff7a7d9b2543c0b
romanchukevichmarina <romanchukevich26@icloud.com>
2023-01-10 23:46:31 +0300

    Создан пустой массив для отсортированного массива файлов, реализовано чтение файлов и создан метод для поиска зависимостей

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
-import java.io.File;
+import java.io.*;
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Objects;
 
@@ -6,17 +7,38 @@ public class Main {
     public static void main(String[] args) {
         File dir = new File("src/Archive/BasicExample");
         ArrayList<File> arr = new ArrayList<>(findAllFiles(dir));
+        ArrayDeque<File> sortedArr = new ArrayDeque<>(arr.size());
+        readFiles(arr, sortedArr);
+    }
+
+    private static void readFiles(ArrayList<File> arr, ArrayDeque<File> sortedArr) {
+        for (File item : Objects.requireNonNull(arr)) {
+            try (BufferedReader br = new BufferedReader(new FileReader(item))) {
+                String s;
+                while ((s = br.readLine()) != null) {
+                    findDependencies(sortedArr, item, s);
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    private static void findDependencies(ArrayDeque<File> sortedArr, File item, String s) {
+        if(s.contains("require")){
+            String[] strArr = s.split(" ");
+            //-----
+            sortedArr.add(item);
+        }
     }
 
     private static ArrayList<File> findAllFiles(File dir) {
         ArrayList<File> arr = new ArrayList<>();
-        if(dir.isDirectory())
-        {
-            for(File item : Objects.requireNonNull(dir.listFiles())){
-                if(item.isDirectory()){
+        if (dir.isDirectory()) {
+            for (File item : Objects.requireNonNull(dir.listFiles())) {
+                if (item.isDirectory()) {
                     findAllFiles(item);
-                }
-                else{
+                } else {
                     arr.add(item);
                 }
             }
