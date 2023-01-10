521843dbc236b2ea6111197869724d7fd3e37716
romanchukevichmarina <romanchukevich26@icloud.com>
2023-01-10 22:56:08 +0300

    Добавлен проход по всем файлам, сформирован ArrayList из них

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,26 @@
 import java.io.File;
+import java.util.ArrayList;
 import java.util.Objects;
 
 public class Main {
     public static void main(String[] args) {
-
-        // определяем объект для каталога
         File dir = new File("src/Archive/BasicExample");
-        // если объект представляет каталог
+        ArrayList<File> arr = new ArrayList<>(findAllFiles(dir));
+    }
+
+    private static ArrayList<File> findAllFiles(File dir) {
+        ArrayList<File> arr = new ArrayList<>();
         if(dir.isDirectory())
         {
-            // получаем все вложенные объекты в каталоге
             for(File item : Objects.requireNonNull(dir.listFiles())){
-
                 if(item.isDirectory()){
-
-                    System.out.println(item.getName() + "  \t folder");
+                    findAllFiles(item);
                 }
                 else{
-
-                    System.out.println(item.getName() + "\t file");
+                    arr.add(item);
                 }
             }
         }
+        return arr;
     }
 }
