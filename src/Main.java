b12313380e9bd429bb8279c252a50b7c4488b63d
romanchukevichmarina <romanchukevich26@icloud.com>
2023-01-11 03:36:57 +0300

    Исправила ошибки в findAllFiles, немного изменила readFile, написала findDependencies без обработки циклических зависимостей

--- src/Main.java
+++ src/Main.java
@@ -1,48 +1,68 @@
 import java.io.*;
-import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Objects;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         File dir = new File("src/Archive/BasicExample");
-        ArrayList<File> arr = new ArrayList<>(findAllFiles(dir));
-        ArrayDeque<File> sortedArr = new ArrayDeque<>(arr.size());
-        readFiles(arr, sortedArr);
+        ArrayList<File> arr = new ArrayList<>();
+        findAllFiles(dir, arr);
+        ArrayList<File> sortedArr = new ArrayList<>(arr.size());
+        ArrayList<String> stringArr = new ArrayList<>(arr.size());
+        for (File item : Objects.requireNonNull(arr)){
+            if(!sortedArr.contains(item))
+                findDependencies(arr, sortedArr, item, readFile(item), stringArr);
+        }
     }
 
-    private static void readFiles(ArrayList<File> arr, ArrayDeque<File> sortedArr) {
-        for (File item : Objects.requireNonNull(arr)) {
+    private static String readFile(File item) {
             try (BufferedReader br = new BufferedReader(new FileReader(item))) {
                 String s;
+                String allStr = "";
                 while ((s = br.readLine()) != null) {
-                    findDependencies(sortedArr, item, s);
+                    allStr = allStr.concat(s);
                 }
+                return allStr;
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
-        }
     }
 
-    private static void findDependencies(ArrayDeque<File> sortedArr, File item, String s) {
-        if(s.contains("require")){
+    private static void findDependencies(ArrayList<File> arr, ArrayList<File> sortedArr, File item, String s, ArrayList<String> stringArr) throws Exception {
+        if (s.contains("require")) {
             String[] strArr = s.split(" ");
-            //-----
-            sortedArr.add(item);
+            int index = 0;
+            for (int i = 0; i < strArr.length; ++i) {
+                if (Objects.equals(strArr[i], "require")) {
+                    String path = strArr[i + 1].substring(1, strArr[i + 1].length() - 1);
+                    File parent = new File(path);
+                    if(!arr.contains(parent)){
+                        throw new Exception("Incorrect require path in file" + item.getName());
+                    }
+                    if(!sortedArr.contains(parent))
+                        findDependencies(arr, sortedArr, parent, readFile(parent), stringArr);
+                    int j = sortedArr.indexOf(parent);
+                    if (index < j)
+                        index = j;
+                }
+            }
+            sortedArr.add(index + 1, item);
+            stringArr.add(index + 1, s);
+        } else {
+            sortedArr.add(0, item);
+            stringArr.add(0, s);
         }
     }
 
-    private static ArrayList<File> findAllFiles(File dir) {
-        ArrayList<File> arr = new ArrayList<>();
+    private static void findAllFiles(File dir, ArrayList<File> arr) {
         if (dir.isDirectory()) {
             for (File item : Objects.requireNonNull(dir.listFiles())) {
                 if (item.isDirectory()) {
-                    findAllFiles(item);
+                    findAllFiles(item, arr);
                 } else {
                     arr.add(item);
                 }
             }
         }
-        return arr;
     }
 }
